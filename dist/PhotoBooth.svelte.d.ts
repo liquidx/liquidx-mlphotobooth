6c6009795f526eec10ba0329d31369e765258543
Alastair Tse <alastair@liquidx.net>
2023-10-19 21:14:15 +0900

    Update dist/

--- dist/PhotoBooth.svelte.d.ts
+++ dist/PhotoBooth.svelte.d.ts
@@ -1,4 +1,4 @@
-import { SvelteComponentTyped } from "svelte";
+import { SvelteComponent } from "svelte";
 declare const __propDef: {
     props: Record<string, never>;
     events: {
@@ -9,6 +9,6 @@ declare const __propDef: {
 export type PhotoBoothProps = typeof __propDef.props;
 export type PhotoBoothEvents = typeof __propDef.events;
 export type PhotoBoothSlots = typeof __propDef.slots;
-export default class PhotoBooth extends SvelteComponentTyped<PhotoBoothProps, PhotoBoothEvents, PhotoBoothSlots> {
+export default class PhotoBooth extends SvelteComponent<PhotoBoothProps, PhotoBoothEvents, PhotoBoothSlots> {
 }
 export {};
